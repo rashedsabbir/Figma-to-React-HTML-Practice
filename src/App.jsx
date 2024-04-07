b9cc8fcf3b88d576720812d12037b464146091c8
rashedsabbir <rashedulhasan1996@gmail.com>
2024-04-07 20:49:09 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Navbar from "./components/navbar";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,14 +10,8 @@ function App() {
   return (
     <>
       <div>
-        <button class="btn btn-info">Info</button>
-        <button class="btn btn-success">Success</button>
-        <button class="btn btn-warning">Warning</button>
-        <button class="btn btn-error">Error</button>
+        <Navbar></Navbar>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
