0297a5b52db340a7d3e49d434410b896ae10d0b1
rashedsabbir <rashedulhasan1996@gmail.com>
2024-09-03 23:01:00 +0600

    pricing card repositioned in webovio

--- React/Webovio/src/components/Pricing/Pricing.jsx
+++ React/Webovio/src/components/Pricing/Pricing.jsx
@@ -4,21 +4,23 @@ const Pricing = () => {
   return (
     <div className="px-40">
       <div className="border-b-2 border-gray-100 mb-24 mt-10 flex items-center justify-center w-[840px]"></div>
-      <section class="w-screen py-20">
-        <h1 class="mb-12 text-center font-sans text-5xl font-bold">Our Blog</h1>
-        <div class="mx-auto grid max-w-screen-lg grid-cols-1 gap-5 p-5 sm:grid-cols-2 md:grid-cols-3 lg:gap-10">
-          <article class="group h-full overflow-hidden rounded-lg border-2 border-gray-200 border-opacity-60 shadow-lg">
-            <div class="py-1 px-6">
-              <h1 class="title-font mb-3 inline-block cursor-pointer text-xl capitali font-extrabold tracking-wide text-gray-800">
-                How to get around in New York
-              </h1>
-              <p class="line-clamp-6 mb-3 cursor-pointer overflow-hidden leading-relaxed text-gray-500">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil,
-                maxime. Accusamus qui incidunt est rem at corrupti, earum fugiat
-                iure.
-              </p>
-            </div>
-          </article>
+      <section class="">
+        <h1 className="font-bold pb-8  text-3xl text-slate-600">
+          Deation & Evaluation - Best Way
+          <br />
+          to Kick off Your Product Idea
+        </h1>
+        <div class="flex items-center justify-center ">
+          <div class="max-w-sm rounded-sm p-6 shadow-[rgba(0,0,15,0.5)_40px_40px_80px_-50px]">
+            <h3 class="mb-2 text-xl font-bold tracking-tight text-slate-800">
+              Extended Shadows Playground
+            </h3>
+            <p class="text-slate-600">
+              Try adjusting the extra shadow utility classes on this card to see
+              what's possible. Learn more about `tailwind-extended-shadows`
+              below:
+            </p>
+          </div>
         </div>
       </section>
     </div>
