60fefeea48e557c3b5be5886731b63837873f050
rashedsabbir <rashedulhasan1996@gmail.com>
2024-09-09 23:17:48 +0600

    footer completed in webovio

--- React/Webovio/src/components/shared/Footer/Footer.jsx
+++ React/Webovio/src/components/shared/Footer/Footer.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-stone-700 text-white px-28">
-      <div className="mx-auto max-w-screen-xl space-y-8 px-4 py-16 sm:px-6 lg:space-y-16 lg:px-8">
-        <div className="flex flex-row justify-between">
+      <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 space-y-6">
+        <div className="flex flex-row justify-between border-y py-16 border-stone-600 border-slim">
           <div className="grid grid-cols-1 gap-20 pr-10 sm:grid-cols-2 border-r border-stone-600 border-slim lg:grid-cols-3">
             <div>
-              <p className="font-medium text-white">Services</p>
+              <p className="font-medium text-white">SERVICES</p>
 
               <ul className="mt-8 space-y-4 text-sm">
                 <li>
@@ -63,7 +63,7 @@ const Footer = () => {
             </div>
 
             <div>
-              <p className="font-medium text-white">Help And Advice</p>
+              <p className="font-medium text-white">HELP AND ADVICE</p>
 
               <ul className="mt-8 space-y-4 text-sm">
                 <li>
@@ -108,7 +108,7 @@ const Footer = () => {
             </div>
 
             <div>
-              <p className="font-medium text-white">Company</p>
+              <p className="font-medium text-white">COMPANY</p>
 
               <ul className="mt-8 space-y-4 text-sm">
                 <li>
@@ -152,45 +152,15 @@ const Footer = () => {
               </ul>
             </div>
           </div>
-          {/* <div className="border-t-2 border-gray-100 flex rotate-90  w-[100px]"></div> */}
-          {/* <div className="divide-x-2 divide-solid divide-black">h</div> */}
+
           <div>
-            <div className="flex flex-row justify-center items-center ">
-              <svg
-                width="55"
-                height="36"
-                viewBox="0 0 55 36"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M36.6502 30.4842C35.35 32.6512 32.2094 32.6511 30.9092 30.4842L12.7815 0.271275L48.8656 0.271275C51.4676 0.271275 53.0748 3.10989 51.7361 5.34111L36.6502 30.4842Z"
-                  fill="#FFD723"
-                />
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M18.1277 30.4842C19.4279 32.6512 22.5685 32.6511 23.8687 30.4842L41.9964 0.271275L5.91239 0.271275C3.31037 0.271275 1.70317 3.10989 3.0419 5.34111L18.1277 30.4842Z"
-                  fill="white"
-                />
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M12.7815 0.27124H41.9964L27.39 24.617L12.7815 0.27124Z"
-                  fill="#F6F6F6"
-                />
-              </svg>
+            <p className="font-medium text-white">GET IN TOUCH</p>
 
-              <a className="btn btn-ghost text-white font-bold text-4xl ml-2">
-                webovio.
-              </a>
-            </div>
-
-            <p className="mt-4 max-w-xs text-gray-500">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse non
-              cupiditate quae nam molestias.
+            <p className="mt-8 max-w-xs text-stone-400">
+              Feel free to get in touch with us via email
+            </p>
+            <p className="mt-6 text-lg font-extrabold text-white">
+              info.webovio@gmail.com
             </p>
 
             <ul className="mt-8 flex gap-6">
@@ -205,7 +175,7 @@ const Footer = () => {
 
                   <svg
                     className="size-6"
-                    fill="currentColor"
+                    fill="#57534e"
                     viewBox="0 0 24 24"
                     aria-hidden="true"
                   >
@@ -225,35 +195,11 @@ const Footer = () => {
                   target="_blank"
                   className="text-stone-400 transition hover:opacity-75"
                 >
-                  <span className="sr-only">Instagram</span>
-
-                  <svg
-                    className="size-6"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                    aria-hidden="true"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </a>
-              </li>
-
-              <li>
-                <a
-                  href="#"
-                  rel="noreferrer"
-                  target="_blank"
-                  className="text-stone-400 transition hover:opacity-75"
-                >
                   <span className="sr-only">Twitter</span>
 
                   <svg
                     className="size-6"
-                    fill="currentColor"
+                    fill="#57534e"
                     viewBox="0 0 24 24"
                     aria-hidden="true"
                   >
@@ -269,35 +215,11 @@ const Footer = () => {
                   target="_blank"
                   className="text-stone-400 transition hover:opacity-75"
                 >
-                  <span className="sr-only">GitHub</span>
-
-                  <svg
-                    className="size-6"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                    aria-hidden="true"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </a>
-              </li>
-
-              <li>
-                <a
-                  href="#"
-                  rel="noreferrer"
-                  target="_blank"
-                  className="text-stone-400 transition hover:opacity-75"
-                >
                   <span className="sr-only">Dribbble</span>
 
                   <svg
                     className="size-6"
-                    fill="currentColor"
+                    fill="#57534e"
                     viewBox="0 0 24 24"
                     aria-hidden="true"
                   >
@@ -312,10 +234,43 @@ const Footer = () => {
             </ul>
           </div>
         </div>
+        <div className="flex flex-row justify-between">
+          <div className="flex flex-row justify-center items-center ">
+            <svg
+              width="32"
+              height="32"
+              viewBox="0 0 55 36"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M36.6502 30.4842C35.35 32.6512 32.2094 32.6511 30.9092 30.4842L12.7815 0.271275L48.8656 0.271275C51.4676 0.271275 53.0748 3.10989 51.7361 5.34111L36.6502 30.4842Z"
+                fill="#FFD723"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M18.1277 30.4842C19.4279 32.6512 22.5685 32.6511 23.8687 30.4842L41.9964 0.271275L5.91239 0.271275C3.31037 0.271275 1.70317 3.10989 3.0419 5.34111L18.1277 30.4842Z"
+                fill="white"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M12.7815 0.27124H41.9964L27.39 24.617L12.7815 0.27124Z"
+                fill="#F6F6F6"
+              />
+            </svg>
 
-        <p className="text-xs text-gray-500">
-          &copy; 2022. Company Name. All rights reserved.
-        </p>
+            <a className="btn btn-ghost text-white font-bold text-2xl ml-2">
+              webovio.
+            </a>
+          </div>
+          <p className="text-xs text-gray-500">
+            &copy; 2020. Company Name. All rights reserved.
+          </p>
+        </div>
       </div>
     </footer>
   );
