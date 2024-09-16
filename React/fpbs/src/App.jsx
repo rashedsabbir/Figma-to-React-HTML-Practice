efb99ad0e8561f9978fa036728b5bcbce83ef182
rashedsabbir <rashedulhasan1996@gmail.com>
2024-09-16 22:40:52 +0600

    tablet added to webovio

--- React/fpbs/src/App.jsx
+++ React/fpbs/src/App.jsx
@@ -2,18 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
+import Header from "./components/header";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div class="relative flex justify-center w-[1250px] h-[700px] border border-8 border-black rounded-2xl">
-        <div class="absolute border border-4  border-black h-full w-full"></div>
-        <span class="border border-black bg-black w-3 h-3 mt-2 rounded-full"></span>
-        <span class="absolute -right-2.5 top-20 border border-2 border-black h-10 rounded-md"></span>
-      </div>
-    </>
+    <div className="mx-36 my-24">
+      <Header />
+    </div>
   );
 }
 
---
+++ React/fpbs/src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const header = () => {
+  return (
+    <div className="border-[1px] border-white rounded-3xl">
+      <div class="relative flex justify-center w-[1000px] h-[700px] border border-8 border-black rounded-3xl">
+        <div class="absolute border border-8  border-black h-full w-full rounded-2xl"></div>
+      </div>
+    </div>
+  );
+};
+
+export default header;
