4dd2106d78503c4e72eb0955a17ca0f6727b8c91
rashedsabbir <rashedulhasan1996@gmail.com>
2024-09-17 22:22:00 +0600

    background image added to header of fpbs

--- React/fpbs/src/Components/Header.jsx
+++ React/fpbs/src/Components/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-
+import img from "../assets/image-1.jpg";
 const header = () => {
   return (
-    <div className="border-[1px] border-white rounded-3xl">
-      <div class="relative flex justify-center w-[1000px] h-[700px] border border-8 border-black rounded-3xl">
-        <div class="absolute border border-8  border-black h-full w-full rounded-2xl"></div>
+    <div className="border-[1px] border-white rounded-3xl bg-black">
+      <div class="relative flex justify-center w-[1000px] h-[700px] border border-8 border-black rounded-3xl ">
+        <div class="absolute pl-1 border border-8  border-black h-full w-full rounded-2xl">
+          <img src={img} className="flex justify-center" alt="" srcset="" />
+        </div>
       </div>
     </div>
   );
