b4cf28d1540f810034c3cc9cfc30911cd8e8517d
rashedsabbir <rashedulhasan1996@gmail.com>
2024-09-20 22:24:13 +0600

    svg icon change to webovio

--- React/fpbs/src/App.jsx
+++ React/fpbs/src/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 import Header from "./components/header";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="lg:mx-36 lg:my-24">
       <Header />
